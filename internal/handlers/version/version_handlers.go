1ed3c114cce39a7f853f223172f4667f0de30e65
Irfan Saf <irfansaf7@gmail.com>
2024-01-26 09:48:57 +0700

    feat(version): version_info done

--- internal/handlers/version/version_handlers.go
+++ internal/handlers/version/version_handlers.go
@@ -1,1 +1,40 @@
 package version
+
+import (
+	"Backend/internal/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Handlers struct {
+	VersionService *services.VersionService
+}
+
+func NewVersionHandlers(versionService *services.VersionService) *Handlers {
+	return &Handlers{VersionService: versionService}
+}
+
+func (h *Handlers) GetVersion(c *gin.Context) {
+	version, err := h.VersionService.GetVersion()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"version": version})
+}
+
+//func (h *Handlers) UpdateVersion(c *gin.Context) {
+//	var version models.Version
+//	if err := c.ShouldBindJSON(&version); err != nil {
+//		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//		return
+//	}
+//
+//	if err := h.VersionService.UpdateVersion(&version); err != nil {
+//		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, gin.H{"version": version})
+//}
