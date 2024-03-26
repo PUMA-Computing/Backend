8ece9a6dac1e79154e66d7ef9e1d782892fccc2b
Irfan Saf <irfansaf7@gmail.com>
2024-03-26 07:23:31 +0700

    fix(cors): fix cors to staging

--- api/routes.go
+++ api/routes.go
@@ -19,7 +19,7 @@ func SetupRoutes() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://104.248.152.47:4000", "http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
